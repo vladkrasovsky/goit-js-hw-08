3c60b17c81dde061b167ae037f4374dc929cfc62
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-09-20 22:34:12 +0300

    Adds 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const refs = {
+  player: document.querySelector('#vimeo-player'),
+};
+
+const player = new Player(refs.player);
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY) || 0);
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate({ seconds }) {
+  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+}
